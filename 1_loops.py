ffcaa71f416a87d7faf50a73960e031f5e797fa5
manny50-cpu <manuelgames30@gmail.com>
2025-12-09 15:01:50 +0000

    working on loops

--- 1_loops.py
+++ 1_loops.py
@@ -1,10 +1,39 @@
 # Example Practice:
-# Print the numbers 1 to 10 using a for loop.
+# Given this list of fruits:
+fruits = ["apple", "banana", "cherry", "date"]
 
 # Challenge:
-# Print the phrase "Keep going!" exactly 5 times.
-
-
+# Use a for loop to print each fruit on a new line.
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+for fruit in fruits:
+    print(fruit)
 # i just worked with loops
 
-# update for joels
+# Given a list of school subjects:
+subjects = ["Math", "Science", "History", "Art"]
+for subject in subjects:
+    print(subject)
+    if subject =="Science":
+        break
+    
+
+  
+
+#print out each subject but stop when you reach history
+# Challenge:
+# Use a for loop and range to print each subject along with its index:
+# Example output: "Subject 0: Math"
+for i in range(len(subjects)):
+    print(f"subject{i}: {subjects[i]}")
+
+# Given:
+numbers = [5, 10, 15, 20]
+
+# Challenge:
+# Use a for loop to add all the numbers and print the total.
+total = 0
+for number in numbers:
+    total+= number
+print(total)
---
+++ pre_lessonloops.py
@@ -0,0 +1,8 @@
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+
+for x in range(1,21):
+    if x==13:
+        continue
+    else:
+        print(x)
