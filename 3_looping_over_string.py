446d4caa27f79263195bc64ead461099cc198ff1
manny50-cpu <manuelgames30@gmail.com>
2025-12-11 14:31:06 +0000

    new vowels system

--- 3_looping_over_string.py
+++ 3_looping_over_string.py
@@ -1,6 +1,17 @@
 # Example Practice:
 # Ask the user for a word.
 # Use a for loop to print each letter on a new line.
-
+word = input("What is the word?")
+vowels = "aeiouAeiou"
+count = 0
+for letter in word:
+    print(letter)
+    
+for  letter in word:
+    if letter in vowels:
+       count+=1  
 # Challenge:
+print("number of vowels is, ", [count])
+
+
 # Count how many vowels are in the word.
